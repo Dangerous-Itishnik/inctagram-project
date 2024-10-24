cefd6d28c3408ecd4367913cbef01140760b3fea
Ihar Voitau <igorgrime@gmail.com>
2024-10-24 23:42:43 +0300

    feat: add header layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
     <>
       <h1>Home page</h1>
-      <Link href={'/signUp'}>signUp</Link>
-      <br />
-      <Link href={'/signIn'}>signIn</Link>
-      <br />
-      <Link href={'/profile'}>profile</Link>
     </>
   )
 }
