0e2f0947c5f92087cd0af2294a83f30237bdf849
Ihar Voitau <igorgrime@gmail.com>
2024-10-24 23:15:47 +0300

    feat: add signUp signIn profile pages and link for them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <h1>Home page</h1>
+  return (
+    <>
+      <h1>Home page</h1>
+      <Link href={'/signUp'}>signUp</Link>
+      <br />
+      <Link href={'/signIn'}>signIn</Link>
+      <br />
+      <Link href={'/profile'}>profile</Link>
+    </>
+  )
 }
