38292f6e02468a207d123a42f162d5e3144e4ffd
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-10-27 13:21:42 +0300

    KAN-20 Create component Button

---
+++ src/app/components/button/Button.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+
+import styles from './button.module.scss'
+
+type Props = {
+  children: ReactNode
+  disabled: boolean
+  type?: 'default' | 'outline' | 'secondary' | 'text'
+}
+
+export const Button = ({ children, disabled, type }: Props) => {
+  let className
+
+  if (type === 'outline') {
+    className = `${styles.button} ${styles.buttonOutline}`
+  } else if (type === 'secondary') {
+    className = `${styles.button} ${styles.buttonSecondary}`
+  } else if (type === 'text') {
+    className = styles.buttonText
+  } else {
+    className = `${styles.button} ${styles.buttonDefault}`
+  }
+
+  return <button className={className} disabled={disabled}>{children}</button>
+}
