0ba68b1c30ce00410f2c75f9b6975328c64c6bec
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-10-29 00:32:03 +0300

    KAN-11 add: sign up

--- src/app/(auth)/signUp/page.tsx
+++ src/app/(auth)/signUp/page.tsx
@@ -1,3 +1,76 @@
+'use client'
+import { useForm } from 'react-hook-form'
+
+import { Input } from '@/app/components/Input/Input'
+import { AuthorizationContainer } from '@/app/components/authorizationContainer/AutoritationContainer'
+import { Button } from '@/app/components/button/Button'
+
+import styles from './styles.module.scss'
+type Props = {
+  Email: string
+  Password: string
+  UserName: string
+}
+
 export default function SignUp() {
-  return <h1>Sign up page</h1>
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<Props>()
+
+  const onSubmit = (data: Props) => {
+    console.log('Submitted data:', data)
+    console.log(errors)
+  }
+
+  return (
+    <AuthorizationContainer>
+      <h2 className={styles.title}>Sign Up</h2>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          errorMessage={errors.UserName?.message}
+          label={'UserName'}
+          {...register('UserName', {
+            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
+            maxLength: { message: 'Максимум 30 символов', value: 30 },
+            minLength: { message: 'Минимум 6 символа', value: 6 },
+            pattern: {
+              message: 'Только латинские буквы',
+              value: /^[A-Za-z0-9_-]+$/,
+            },
+            required: 'Поле обязательно для заполнения',
+          })}
+        />
+        <Input
+          errorMessage={errors.Email?.message}
+          label={'Email'}
+          {...register('Email', {
+            pattern: {
+              message: 'Введите корректный email',
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            },
+            required: 'Поле обязательно для заполнения',
+          })}
+        />
+        <Input
+          errorMessage={errors.Password?.message}
+          label={'Password'}
+          {...register('Password', {
+            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
+            maxLength: { message: 'Максимум 20 символов', value: 20 },
+            minLength: { message: 'Минимум 6 символа', value: 6 },
+            pattern: {
+              message: 'Только латинские буквы, цифры и специальные символы',
+              value: /^[A-Za-z0-9!"#$%&'()*+,\-.\/:;<=>?@[\\\]^_`{|}~]+$/,
+            },
+            required: 'Поле обязательно для заполнения',
+          })}
+        />
+        //TODO сделать кнопку правильно //TODO Доделать валидацию
+        <button type={'submit'}>Отправить</button>
+        <Button disabled={false}>Sign Up</Button>
+      </form>
+    </AuthorizationContainer>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,73 +1,9 @@
-'use client'
-import { useForm } from 'react-hook-form'
-
-import { Input } from '@/app/components/Input/Input'
-
 import '@/styles/index.scss'
 
-type FormValues = {
-  Email: string
-  Password: string
-  UserName: string
-}
-
 export default function Home() {
-  const {
-    formState: { errors },
-    handleSubmit,
-    register,
-  } = useForm<FormValues>()
-
-  const onSubmit = (data: FormValues) => {
-    console.log('Submitted data:', data)
-    console.log(errors)
-  }
-
   return (
     <>
       <h1>Home page</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          errorMessage={errors.UserName?.message}
-          label={'UserName'}
-          {...register('UserName', {
-            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
-            maxLength: { message: 'Максимум 30 символов', value: 30 },
-            minLength: { message: 'Минимум 6 символа', value: 6 },
-            pattern: {
-              message: 'Только латинские буквы',
-              value: /^[A-Za-z0-9_-]+$/,
-            },
-            required: 'Поле обязательно для заполнения',
-          })}
-        />
-        <Input
-          errorMessage={errors.Email?.message}
-          label={'Email'}
-          {...register('Email', {
-            pattern: {
-              message: 'Введите корректный email',
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            },
-            required: 'Поле обязательно для заполнения',
-          })}
-        />
-        <Input
-          errorMessage={errors.Password?.message}
-          label={'Password'}
-          {...register('Password', {
-            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
-            maxLength: { message: 'Максимум 20 символов', value: 20 },
-            minLength: { message: 'Минимум 6 символа', value: 6 },
-            pattern: {
-              message: 'Только латинские буквы, цифры и специальные символы',
-              value: /^[A-Za-z0-9!"#$%&'()*+,\-.\/:;<=>?@[\\\]^_`{|}~]+$/,
-            },
-            required: 'Поле обязательно для заполнения',
-          })}
-        />
-        <button type={'submit'}>Отправить</button>
-      </form>
     </>
   )
 }
