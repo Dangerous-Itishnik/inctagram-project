1484aa606554d5e41a374aa76e27771d5cf0ca0e
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-10-28 23:49:32 +0300

    KAN-11 fix input: refresh validate and create input

--- src/app/components/Input/Input.tsx
+++ src/app/components/Input/Input.tsx
@@ -1,61 +1,62 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { forwardRef, useState } from 'react'
 
 import styles from './input.module.scss'
 
 export type InputProps = {
   errorMessage?: string
   label: string
-  onChangeHandler: (value: string) => void
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
   onFocus?: () => void
   type?: 'password' | 'search' | 'text'
-  value: string
 } & React.HTMLAttributes<HTMLInputElement>
 
-export const Input = ({
-  errorMessage,
-  label,
-  onChangeHandler,
-  onFocus,
-  type,
-  value,
-}: InputProps) => {
-  const [showText, setShowText] = useState(false)
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ errorMessage, label, onChange, onFocus, type, ...inputProps }, ref) => {
+    const [showText, setShowText] = useState(false)
 
-  const togglePasswordVisibility = () => {
-    setShowText(prev => !prev)
-  }
+    const togglePasswordVisibility = () => {
+      setShowText(prev => !prev)
+    }
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    onChangeHandler(event.currentTarget.value)
-  }
-  const classNameInput = type === 'search' ? `${styles.input} ${styles.inputSearch}` : styles.input
-  const inputType = showText ? 'password' : 'text'
-  let buttonType
+    const classNameInput =
+      type === 'search' ? `${styles.input} ${styles.inputSearch}` : styles.input
+    const inputType = type === 'password' && showText ? 'password' : 'text'
+    let buttonType
 
-  if (type === 'password') {
-    buttonType = <button className={styles.button} onClick={togglePasswordVisibility}></button>
-  }
-  if (type === 'search') {
-    buttonType = <button className={styles.buttonSearch} onClick={() => {}}></button>
-  }
+    if (type === 'password') {
+      buttonType = (
+        <button
+          className={styles.button}
+          onClick={togglePasswordVisibility}
+          type={'button'}
+        ></button>
+      )
+    }
+    if (type === 'search') {
+      buttonType = (
+        <button className={styles.buttonSearch} onClick={() => {}} type={'button'}></button>
+      )
+    }
 
-  return (
-    <div className={`${styles.container} ${errorMessage && styles.error}`}>
-      <label className={styles.label} htmlFor={label}>
-        {label}
-      </label>
-      <input
-        className={classNameInput}
-        name={label}
-        onChange={onChange}
-        onFocus={onFocus}
-        type={inputType}
-        value={value}
-      ></input>
-      {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
-      {buttonType}
-    </div>
-  )
-}
+    return (
+      <div className={`${styles.container} ${errorMessage && styles.error}`}>
+        <label className={styles.label} htmlFor={label}>
+          {label}
+        </label>
+        <input
+          ref={ref}
+          {...inputProps}
+          className={classNameInput}
+          name={label}
+          onChange={onChange}
+          onFocus={onFocus}
+          type={inputType}
+        ></input>
+        {errorMessage && <p className={styles.errorMessage}>{errorMessage}</p>}
+        {buttonType}
+      </div>
+    )
+  }
+)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
 'use client'
-import { Controller, useForm } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 import { Input } from '@/app/components/Input/Input'
-import { register } from 'next/dist/client/components/react-dev-overlay/pages/client'
 
 import '@/styles/index.scss'
 
 type FormValues = {
+  Email: string
+  Password: string
   UserName: string
 }
 
 export default function Home() {
   const {
-    clearErrors,
-    control,
     formState: { errors },
     handleSubmit,
+    register,
   } = useForm<FormValues>()
 
   const onSubmit = (data: FormValues) => {
@@ -27,35 +27,45 @@ export default function Home() {
     <>
       <h1>Home page</h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          control={control}
-          defaultValue={''}
-          name={'UserName'}
-          render={({ field }) => (
-            <Input
-              errorMessage={errors.UserName?.message}
-              label={'UserName'}
-              onChangeHandler={(value: string) => {
-                field.onChange(value)
-                clearErrors('UserName')
-              }}
-              onFocus={() => clearErrors('UserName')}
-              value={field.value}
-            />
-          )}
-          rules={{
-            maxLength: {
-              message: 'Минимум 6 символа',
-              value: 30,
-            },
-            minLength: {
-              message: 'Минимум 6 символа',
-              value: 6,
+        <Input
+          errorMessage={errors.UserName?.message}
+          label={'UserName'}
+          {...register('UserName', {
+            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
+            maxLength: { message: 'Максимум 30 символов', value: 30 },
+            minLength: { message: 'Минимум 6 символа', value: 6 },
+            pattern: {
+              message: 'Только латинские буквы',
+              value: /^[A-Za-z0-9_-]+$/,
             },
             required: 'Поле обязательно для заполнения',
-          }}
+          })}
         />
-
+        <Input
+          errorMessage={errors.Email?.message}
+          label={'Email'}
+          {...register('Email', {
+            pattern: {
+              message: 'Введите корректный email',
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            },
+            required: 'Поле обязательно для заполнения',
+          })}
+        />
+        <Input
+          errorMessage={errors.Password?.message}
+          label={'Password'}
+          {...register('Password', {
+            //TODO бага с maxLength что бы работало надо поставить ниже minLength. При сохранении происходит изменение
+            maxLength: { message: 'Максимум 20 символов', value: 20 },
+            minLength: { message: 'Минимум 6 символа', value: 6 },
+            pattern: {
+              message: 'Только латинские буквы, цифры и специальные символы',
+              value: /^[A-Za-z0-9!"#$%&'()*+,\-.\/:;<=>?@[\\\]^_`{|}~]+$/,
+            },
+            required: 'Поле обязательно для заполнения',
+          })}
+        />
         <button type={'submit'}>Отправить</button>
       </form>
     </>
