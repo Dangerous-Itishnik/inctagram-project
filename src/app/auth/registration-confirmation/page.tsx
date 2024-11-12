da0f08dab49da16fb7a7907f21057bb3fd014e48
Ihar Voitau <igorgrime@gmail.com>
2024-11-12 14:27:41 +0300

    fix: RegistrationConfirmationPage logic

--- src/app/auth/registration-confirmation/page.tsx
+++ src/app/auth/registration-confirmation/page.tsx
@@ -4,19 +4,30 @@ import { useEffect } from 'react'
 
 import RegistrationConfirmation from '@/features/auth/registrationConfirmation/ui/registrationConfirmation'
 import { useRegistrationConfirmationMutation } from '@/features/auth/signUp/api/signUpApi'
-import { useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 export default function RegistrationConfirmationPage() {
   const searchParams = useSearchParams()
-  const { code: confirmationCode } = Object.fromEntries(searchParams.entries())
+  const confirmationCode = searchParams.get('code')
+  const router = useRouter()
 
   const [registrationConfirmation] = useRegistrationConfirmationMutation()
 
   useEffect(() => {
-    if (confirmationCode) {
-      registrationConfirmation({ confirmationCode })
+    const isConfirmed = localStorage.getItem('registrationConfirmed') === confirmationCode
+
+    if (confirmationCode && !isConfirmed) {
+      registrationConfirmation({ confirmationCode }).then(() => {
+        localStorage.setItem('registrationConfirmed', confirmationCode)
+      })
     }
   }, [confirmationCode, registrationConfirmation])
+
+  if (!confirmationCode) {
+    router.push('/signUp')
+
+    return null
+  }
 
   return <RegistrationConfirmation />
 }
