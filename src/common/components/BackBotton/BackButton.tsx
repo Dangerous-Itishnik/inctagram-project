8d6b66c7bf19effe2a64040427cbe364d42efbb2
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2024-11-25 18:46:14 +0300

    KAN-7: added scripts for icons. Added icons. Added component BackButton

--- src/common/components/BackBotton/BackButton.tsx
+++ src/common/components/BackBotton/BackButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { ArrowBackOutline } from '@/assets/icons/components'
 import { useRouter } from 'next/navigation'
 
 import s from './backButton.module.scss'
@@ -16,8 +17,11 @@ export const BackButton = ({ title }: Props) => {
   }
 
   return (
-    <button className={s.backButton} onClick={backPage} type={'button'}>
-      {title}
-    </button>
+    <div className={s.backButtonBox}>
+      <ArrowBackOutline />
+      <button className={s.backButton} onClick={backPage} type={'button'}>
+        {title}
+      </button>
+    </div>
   )
 }
