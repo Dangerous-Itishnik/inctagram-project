aafe233160530b6cc2eca229191a1b1c1a1706c0
Ihar Voitau <igorgrime@gmail.com>
2024-12-01 23:46:00 +0300

    ref: add redirect uri for prod

--- src/common/components/GoogleAuthButton/GoogleAuthButton.tsx
+++ src/common/components/GoogleAuthButton/GoogleAuthButton.tsx
@@ -4,7 +4,7 @@ export const GoogleAuthButton = () => {
   const loginGoogle = () => {
     //TODO спрятать в env
     window.location.href =
-      'https://accounts.google.com/o/oauth2/v2/auth?client_id=272583913867-t74i019ufdvmarh05jlv8bcu1ak0a6o6.apps.googleusercontent.com&redirect_uri=http://localhost:3000&response_type=code&scope=profile%20email'
+      'https://accounts.google.com/o/oauth2/v2/auth?client_id=272583913867-t74i019ufdvmarh05jlv8bcu1ak0a6o6.apps.googleusercontent.com&redirect_uri=https://dang-inc-project.uk&response_type=code&scope=profile%20email'
   }
 
   return (
