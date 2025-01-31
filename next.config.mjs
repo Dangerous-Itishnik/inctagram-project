38a77651023a9d43852804cd7d4e699af4bdbf52
Natalia <natalia21080@yahoo.de>
2025-01-31 14:46:39 +0100

    KAN-184 ci: add AWS config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,28 @@ const nextConfig = {
     missingSuspenseWithCSRBailout: false,
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.yandexcloud.net',
+        port: '',
+        pathname: '/users-inctagram/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/trainee-instagram-api/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+        port: '',
+        pathname: '/trainee-instagram-api/Image/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
