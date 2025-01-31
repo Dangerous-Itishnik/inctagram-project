00315b8c973f6a31f43f3523189d85faa1d05a3b
Natalia <natalia21080@yahoo.de>
2025-01-31 14:47:31 +0100

    KAN-184 feat: GET request

--- src/service/posts/posts.service.ts
+++ src/service/posts/posts.service.ts
@@ -1,85 +1,105 @@
-import { baseApi } from '@/service/baseApi'
+import { baseApi } from "@/service/baseApi";
+
 
 export const postsApi = baseApi.injectEndpoints({
-  endpoints: build => ({
-    getPosts: build.query<GetImages[], PostsId>({
-      query: ({userId, postId}) => {
-        return {
-          method: 'GET',
-          url: `/api/v1/public-posts/user/${userId}/${postId}?pageSize=8`
-        }
-      },
-      providesTags: ['Posts']
-
-    }),
-    // deletePost: build.mutation<
-    //   any,
-    //   {
-    //     accessToken: string | undefined
-    //     postId: number
-    //   }
-    // >({
-    //   query: ({ accessToken, postId }) => ({
-    //     method: 'DELETE',
-    //     url: `/posts/${postId}`,
-    //   }),
-    // }),
-    postImage: build.mutation<Response, FormData>({
-      query: images => ({
-        body: images,
-        method: 'POST',
-        url: '/api/v1/posts/image',
+  tagTypes: ['Posts'],
+  endpoints: build => {
+    return ({
+      getPosts: build.query<PostsAllData, { userId, endCursorPostId}>({
+        providesTags: ['Posts'],
+        query: ({ userId, endCursorPostId }) => ({
+          method: "GET",
+          url: `api/v1/public-posts/user/${userId}/${endCursorPostId || ''}?pageSize=8`,
+        }),
       }),
-    }),
-    postPost: build.mutation({
-      query: data => ({
-        body: data,
-        method: 'POST',
-        url: '/api/v1/posts',
-      }),
-    }),
-    // updatePost: build.mutation<
-    //   { description: 'string' },
-    //   {
-    //     accessToken: string | undefined
-    //     description: string
-    //     postId: number
-    //   }
-    // >({
-    //   //TODO для того что бы обновить посты и отрисовать
-    //   // invalidatesTags: ['Posts'],
-    //   query: ({ accessToken, description, postId }) => ({
-    //     body: { description },
-    //     method: 'PUT',
-    //     url: `/posts/${postId}`,
-    //   }),
-    // }),
-  }),
-})
 
-export const { usePostImageMutation, usePostPostMutation, useGetPostQuery } = postsApi
+      postImage: build.mutation<Response, FormData>({
+        query: images => ({
+          body: images,
+          method: "POST",
+          url: "/api/v1/posts/image"
+        })
+      }),
+      postPost: build.mutation({
+        query: data => ({
+          body: data,
+          method: "POST",
+          url: "/api/v1/posts"
+        })
+      })
+    });
+  }
+});
+
+
+// deletePost: build.mutation<
+//   any,
+//   {
+//     accessToken: string | undefined
+//     postId: number
+//   }
+// >({
+//   query: ({ accessToken, postId }) => ({
+//     method: 'DELETE',
+//     url: `/posts/${postId}`,
+//   }),
+// }),
+
+// updatePost: build.mutation<
+//   { description: 'string' },
+//   {
+//     accessToken: string | undefined
+//     description: string
+//     postId: number
+//   }
+// >({
+//   //TODO для того что бы обновить посты и отрисовать
+//   // invalidatesTags: ['Posts'],
+//   query: ({ accessToken, description, postId }) => ({
+//     body: { description },
+//     method: 'PUT',
+//     url: `/posts/${postId}`,
+//   }),
+// }),
+
+
+export const { usePostImageMutation, usePostPostMutation, useGetPostsQuery } = postsApi;
 
 export type Response = {
   images: ResponseImages[]
+  description?: string
 }
 export type ResponseImages = {
-  createdAt: string
-  fileSize: number
-  height: number
-  uploadId: string
+  id: number
+  createdAt?: string
+  fileSize?: number
+  height?: number
+  uploadId: number
   url: string
-  width: number
+  width?: number
 }
 
-type GetImages = {
+
+export type PostItem = {
   id: number,
-  url: string,
+  ownerId: number,
+  userName: string,
   description: string,
-  width: number,
-  height: number
+  location: string,
+  images: ResponseImages[],
+  createdAt: string,
+  updatedAt: string,
+  avatarOwner: string,
+  owner: {
+    firstName: string,
+    lastName: string,
+  }
+}
+export type PostsAllData = {
+  totalCount: number,
+  pageSize: number,
+  items: PostItem[],
+  totalUsers: number
 }
 
-export type PostsId = {
-  postId: number
-  userId: number
-}
+
