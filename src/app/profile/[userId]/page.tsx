293c4bce2d35c82af6e261d34e66090e028d1fd0
Natalia <natalia21080@yahoo.de>
2025-01-31 14:50:52 +0100

    KAN-184 feat: imagePresentation

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,9 +1,16 @@
 'use client'
 
+import ImagePostLists from "@/common/components/ImagePost/ImagePostLists";
+import { Header } from "@/common/components/Header/Header";
+import { useState } from "react";
+
 export default function Profile() {
+
+
   return (
     <div>
       <h1>Profile Page</h1>
+      <ImagePostLists/>
     </div>
   )
 }
