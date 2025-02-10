fa457e0d0637190a4415a953f18d71ff511931e0
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-10 14:49:18 +0300

    refactor: add to the header Dropdown

---
+++ src/common/components/DropDownMenuHeader/DropDownMenuHeader.tsx
@@ -0,0 +1,55 @@
+import { HTMLProps, useState } from 'react'
+
+import { BookmarkOutline, LogOutOutline, TrendingUpOutline } from '@/assets/icons/components'
+import Link from 'next/link'
+
+import styles from './DropDownMenuHeader.module.scss' // Путь к вашему SCSS файлу
+
+type Props = {
+  openPopUp: () => void
+} & HTMLProps<HTMLDivElement>
+export const DropDownMenuHeader = ({ openPopUp }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(prevState => !prevState)
+  }
+
+  const closeMenu = () => {
+    setIsOpen(false)
+  }
+
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.trigger} onClick={toggleMenu}>
+        <span className={isOpen ? styles.isActive : ''}></span>
+      </button>
+
+      {isOpen && (
+        <div className={`${styles.content} ${isOpen ? styles.show : ''}`}>
+          <ul>
+            <li onClick={closeMenu}>
+              <Link className={styles.link} href={''}>
+                <TrendingUpOutline />
+                <span>Statistics</span>
+              </Link>
+            </li>
+            <li onClick={closeMenu}>
+              {' '}
+              <Link className={styles.link} href={''}>
+                <BookmarkOutline />
+                <span>Favorites</span>
+              </Link>
+            </li>
+            <li onClick={closeMenu}>
+              <button className={styles.link} onClick={openPopUp} type={'button'}>
+                <LogOutOutline />
+                <span>Log Out</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  )
+}
