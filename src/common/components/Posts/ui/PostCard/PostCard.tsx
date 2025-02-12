d6a65cd20c9a801ac3697311475295478cea0743
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-12 11:23:05 +0300

    KAN-86 refactor: decomposition components

---
+++ src/common/components/Posts/ui/PostCard/PostCard.tsx
@@ -0,0 +1,41 @@
+import { Post } from '@/common/components/Posts/model/postsType'
+import { TextWithShowMore } from '@/common/components/Posts/ui/PostCard/TextWithShowMore/TextWithShowMore'
+import { formatDistanceToNow } from 'date-fns/formatDistanceToNow'
+import { ru } from 'date-fns/locale/ru'
+import Image from 'next/image'
+
+import styles from '@/common/components/Posts/ui/PostCard/PostCard.module.scss'
+
+type Props = {
+  post: Post
+}
+
+export const PostCard = ({ post }: Props) => {
+  const { avatarOwner, description, images, userName } = post
+
+  const timeAgo = (date: string): string => {
+    const parsedDate = new Date(date)
+
+    return formatDistanceToNow(parsedDate, { addSuffix: true, locale: ru })
+  }
+
+  return (
+    <button className={styles.post} type={'button'}>
+      <div className={styles.slider}>
+        <Image alt={''} className={styles.image} fill src={images[0].url} />
+      </div>
+      <div className={styles.userProfile}>
+        <Image
+          alt={'Avatar'}
+          className={styles.photoUser}
+          height={36}
+          src={avatarOwner}
+          width={36}
+        />
+        <span className={styles.nameUser}>{userName}</span>
+      </div>
+      <div className={styles.ublicationDate}>{timeAgo(post.createdAt)}</div>
+      <TextWithShowMore text={description === '' ? 'Описания нетрстку' : description} />
+    </button>
+  )
+}
--- src/common/components/Posts/ui/Posts.tsx
+++ src/common/components/Posts/ui/Posts.tsx
@@ -1,7 +1,6 @@
+import { Post } from '@/common/components/Posts/model/postsType'
+import { PostCard } from '@/common/components/Posts/ui/PostCard/PostCard'
 import { Spinner } from '@radix-ui/themes'
-import { formatDistanceToNow } from 'date-fns/formatDistanceToNow'
-import { ru } from 'date-fns/locale/ru'
-import Image from 'next/image'
 
 import styles from './Posts.module.scss'
 
@@ -10,75 +9,9 @@ type PostsProps = {
 }
 
 export const Posts = ({ posts }: PostsProps) => {
-  const timeAgo = (date: string): string => {
-    const parsedDate = new Date(date)
-
-    return formatDistanceToNow(parsedDate, { addSuffix: true, locale: ru })
-  }
-
   return (
     <div className={styles.posts}>
-      {posts ? (
-        posts.map(post => {
-          const { avatarOwner, description, id, images, userName } = post
-
-          return (
-            <button className={styles.post} key={id} type={'button'}>
-              <div className={styles.slider}>
-                <Image alt={''} className={styles.image} fill src={images[0].url} />
-              </div>
-              <div className={styles.userProfile}>
-                <Image
-                  alt={'Avatar'}
-                  className={styles.photoUser}
-                  height={36}
-                  src={avatarOwner}
-                  width={36}
-                />
-                <span className={styles.nameUser}>{userName}</span>
-              </div>
-              <div className={styles.ublicationDate}>{timeAgo(post.createdAt)}</div>
-              <p className={styles.text}>
-                {description === ''
-                  ? 'Нету описания. Поэтому стоит мое описанние, для того что бы проверить верстку'
-                  : description}
-              </p>
-            </button>
-          )
-        })
-      ) : (
-        <Spinner />
-      )}
+      {posts ? posts.map(post => <PostCard key={post.id} post={post} />) : <Spinner />}
     </div>
   )
-}
-
-export type ImagePostResponse = {
-  createdAt: string
-  fileSize: number
-  height: number
-  uploadId: string
-  url: string
-  width: number
-}
-
-export type Owner = {
-  firstName: string
-  lastName: string
-}
-
-export type Post = {
-  avatarOwner: string
-  avatarWhoLikes: boolean
-  createdAt: string
-  description: string
-  id: number
-  images: ImagePostResponse[]
-  isLiked: boolean
-  likesCount: number
-  location: string
-  owner: Owner
-  ownerId: number
-  updatedAt: string
-  userName: string
 }
