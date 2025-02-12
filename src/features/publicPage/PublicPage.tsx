ceaa1c5ae66ea423d7913d4d3f5d1806cb9c8240
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-12 11:54:48 +0300

    KAN-86 refactor: style layout

--- src/features/publicPage/PublicPage.tsx
+++ src/features/publicPage/PublicPage.tsx
@@ -2,13 +2,14 @@ import { Posts } from '@/common/components/Posts/ui/Posts'
 import { TotalUsers } from '@/common/components/TotalUsers/ui/TotalUsers'
 import { usePostAllQuery } from '@/service/publicPosts/pablicPosts.service'
 
+import styles from './PublicPage.module.scss'
 export const PublicPage = () => {
   const { data } = usePostAllQuery({ pageSize: 4 })
 
   return (
-    <>
+    <section className={styles.publicPage}>
       <TotalUsers />
       <Posts posts={data && data.items} />
-    </>
+    </section>
   )
 }
