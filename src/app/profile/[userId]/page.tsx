f8c2bb74230d43c4cf1550153a69898e03d09518
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-15 17:44:40 +0300

    KAN-93 fix: bag Spinner

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import ProfileHeader from '@/features/profilePage/ProfileHeadeer/ProfileHeader'
+import { useGetPostQuery } from '@/service/posts/posts.service'
 import { useProfileUserQuery } from '@/service/publicUsers/publicUsers.service'
 import { Spinner } from '@radix-ui/themes'
 import { useParams } from 'next/navigation'
@@ -8,15 +9,19 @@ import { useParams } from 'next/navigation'
 export default function Profile() {
   const params = useParams()
   const profileId = +params.userId // id из URL
-  const { data } = useProfileUserQuery({ profileId })
+  const { data: profileUserData, isLoading: isLoadingProfile } = useProfileUserQuery({ profileId })
+  const { data: postsData, isLoading: isLoadingPost } = useGetPostQuery({ userName: 'adsasdasd' })
 
-  if (!data) {
+  if (isLoadingProfile || isLoadingPost) {
     return <Spinner />
+  }
+  if (!profileUserData || !postsData) {
+    return <div>Данные не подгрузились</div>
   }
 
   return (
     <div>
-      <ProfileHeader profileUser={data} />
+      <ProfileHeader profileUser={profileUserData} />
     </div>
   )
 }
