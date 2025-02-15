2cbe1bc6a9330b0a411674aca681a8ab8633c05c
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-02-15 16:59:45 +0300

    KAN-93 feat: create ProfileApi

--- src/app/profile/[userId]/page.tsx
+++ src/app/profile/[userId]/page.tsx
@@ -1,9 +1,22 @@
 'use client'
 
+import ProfileHeader from '@/features/profilePage/ProfileHeadeer/ProfileHeader'
+import { useProfileUserQuery } from '@/service/publicUsers/publicUsers.service'
+import { Spinner } from '@radix-ui/themes'
+import { useParams } from 'next/navigation'
+
 export default function Profile() {
+  const params = useParams()
+  const profileId = +params.userId // id из URL
+  const { data } = useProfileUserQuery({ profileId })
+
+  if (!data) {
+    return <Spinner />
+  }
+
   return (
     <div>
-      <h1>Profile Page</h1>
+      <ProfileHeader profileUser={data} />
     </div>
   )
 }
---
+++ src/features/profilePage/ProfileHeadeer/ProfileHeader.tsx
@@ -0,0 +1,54 @@
+import { Typography } from '@/common/components/Typography'
+import { ProfileUserResponse } from '@/service/publicUsers/publicUsers.service'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import styles from './profileHeader.module.scss'
+type Props = {
+  profileUser: ProfileUserResponse
+}
+const ProfileHeader = ({ profileUser }: Props) => {
+  const { aboutMe, avatars, userMetadata, userName } = profileUser
+
+  return (
+    <div className={styles.headerContainer}>
+      <div className={styles.imageContainer}>
+        <Image
+          alt={`UserPhoto`}
+          height={204}
+          priority
+          src={avatars.length ? avatars[0].url : ''}
+          width={204}
+        />
+      </div>
+      <div className={styles.userNameContainer}>
+        <div>
+          <Link href={'general-info//???'}>
+            <Typography variant={'h3'}>{userName}</Typography>
+          </Link>
+        </div>
+        <div className={styles.infoContainer}>
+          <div>
+            <Typography variant={'h3'}>{userMetadata.following}</Typography>
+          </div>
+          <div>
+            <Typography variant={'h3'}>{userMetadata.followers}</Typography>
+          </div>
+          <div>
+            <Typography variant={'h3'}>{userMetadata.publications}</Typography>
+          </div>
+        </div>
+        <div>
+          <Typography variant={'body2'}>
+            {aboutMe ? aboutMe : 'Описания нет'}
+            <span className={styles.spanText}>
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </span>
+          </Typography>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileHeader
