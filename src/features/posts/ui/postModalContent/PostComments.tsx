0f1823afeb9329e8fbd3e0f871f1b14a943aa35b
HAROSHII <igorgrime@gmail.com>
2025-03-18 13:29:53 +0300

    test

--- src/features/posts/ui/postModalContent/PostComments.tsx
+++ src/features/posts/ui/postModalContent/PostComments.tsx
@@ -1,5 +1,11 @@
 const PostComments = () => {
-  return <div></div>
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui at aperiam quos iste maiores ut
+      iure, esse rem! Voluptatibus aspernatur itaque est enim, iste repudiandae quas minima dicta
+      excepturi odit.
+    </div>
+  )
 }
 
 export default PostComments
