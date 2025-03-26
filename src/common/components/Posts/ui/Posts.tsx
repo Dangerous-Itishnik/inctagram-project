74ef540848dd0751bf1f192c42e85f9a13effdda
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-03-26 13:35:10 +0300

    fix: change PublicPage components to the server component

--- src/common/components/Posts/ui/Posts.tsx
+++ src/common/components/Posts/ui/Posts.tsx
@@ -1,17 +1,18 @@
 import { PostCard } from '@/common/components/Posts/ui/PostCard/PostCard'
 import { Post } from '@/service/posts/post.type'
-import { Spinner } from '@radix-ui/themes'
 
 import styles from './Posts.module.scss'
 
 type PostsProps = {
-  posts: Post[] | undefined
+  posts: Post[]
 }
 
 export const Posts = ({ posts }: PostsProps) => {
   return (
     <div className={styles.posts}>
-      {posts ? posts.map(post => <PostCard key={post.id} post={post} />) : <Spinner />}
+      {posts.map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </div>
   )
 }
--- src/features/publicPage/PublicPage.tsx
+++ src/features/publicPage/PublicPage.tsx
@@ -1,21 +1,56 @@
+import { BASE_URL } from '@/common/api/common.api'
 import { Posts } from '@/common/components/Posts/ui/Posts'
 import { TotalUsers } from '@/common/components/TotalUsers/ui/TotalUsers'
-import { usePostAllQuery } from '@/service/publicPosts/publicPosts.service'
-import { Spinner } from '@radix-ui/themes'
+import { PostsAll, Response } from '@/service/publicPosts/publicPosts.service'
 
 import styles from './PublicPage.module.scss'
 
-export const PublicPage = () => {
-  const { data, isLoading } = usePostAllQuery({ pageSize: 4 })
+export const dynamic = 'force-static'
 
-  if (isLoading || !data) {
-    return <Spinner />
+async function fetchTotalUsers() {
+  const response = await fetch(`${BASE_URL}/api/v1/public-user`, {
+    next: { revalidate: 60 },
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch profile user data')
   }
 
-  return (
-    <section className={styles.publicPage}>
-      <TotalUsers />
-      {data?.items ? <Posts posts={data && data.items} /> : 'Посты не подгрузились'}
-    </section>
-  )
+  return response.json() as Promise<{ totalCount: number }>
+}
+
+async function fetchPosts(params: PostsAll) {
+  const queryString = new URLSearchParams(params as Record<string, string>).toString()
+
+  const response = await fetch(`${BASE_URL}/api/v1/public-posts/all?${queryString}`, {
+    method: 'GET', // Явно указываем GET
+    next: { revalidate: 60 },
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch public posts')
+  }
+
+  return response.json() as Promise<Response>
+}
+
+export const PublicPage = async () => {
+  try {
+    const dataTotalUser = await fetchTotalUsers()
+    const dataPosts = await fetchPosts({ pageSize: 4 })
+
+    return (
+      <section className={styles.publicPage}>
+        <TotalUsers totalCount={dataTotalUser.totalCount} />
+        <Posts posts={dataPosts.items} />
+      </section>
+    )
+  } catch {
+    return (
+      <section className={styles.publicPage}>
+        <TotalUsers totalCount={0} />
+        <div>Данные не загрузились</div>
+      </section>
+    )
+  }
 }
