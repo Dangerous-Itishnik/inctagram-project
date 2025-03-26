9c4fe7bbfd445f262232a6c29fa2259d202a3a50
Sanyo4ek952 <Sanyo4ek952@gmail.com>
2025-03-26 15:31:14 +0300

    fix: many requests

--- src/features/publicPage/PublicPage.tsx
+++ src/features/publicPage/PublicPage.tsx
@@ -1,48 +1,53 @@
 import { BASE_URL } from '@/common/api/common.api'
 import { Posts } from '@/common/components/Posts/ui/Posts'
 import { TotalUsers } from '@/common/components/TotalUsers/ui/TotalUsers'
-import { PostsAll, Response } from '@/service/publicPosts/publicPosts.service'
+import { PostsAll } from '@/service/publicPosts/publicPosts.service'
 
 import styles from './PublicPage.module.scss'
 
 export const dynamic = 'force-static'
 
-async function fetchTotalUsers() {
-  const response = await fetch(`${BASE_URL}/api/v1/public-user`, {
-    next: { revalidate: 60 },
-  })
+const fetchData = {
+  posts: async (params: PostsAll) => {
+    const query = new URLSearchParams(params as Record<string, string>)
+    const res = await fetch(`${BASE_URL}/api/v1/public-posts/all?${query}`, {
+      cache: 'force-cache',
+      method: 'GET',
+      next: { revalidate: 60, tags: ['posts'] },
+    })
 
-  if (!response.ok) {
-    throw new Error('Failed to fetch profile user data')
-  }
+    if (!res.ok) {
+      throw new Error('Failed to fetch posts')
+    }
 
-  return response.json() as Promise<{ totalCount: number }>
-}
+    return res.json()
+  },
+  totalUsers: async () => {
+    const res = await fetch(`${BASE_URL}/api/v1/public-user`, {
+      cache: 'force-cache',
+      method: 'GET',
+      next: { revalidate: 60, tags: ['total-users'] },
+    })
 
-async function fetchPosts(params: PostsAll) {
-  const queryString = new URLSearchParams(params as Record<string, string>).toString()
+    if (!res.ok) {
+      throw new Error('Failed to fetch total users')
+    }
 
-  const response = await fetch(`${BASE_URL}/api/v1/public-posts/all?${queryString}`, {
-    method: 'GET', // Явно указываем GET
-    next: { revalidate: 60 },
-  })
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch public posts')
-  }
-
-  return response.json() as Promise<Response>
+    return res.json()
+  },
 }
 
 export const PublicPage = async () => {
   try {
-    const dataTotalUser = await fetchTotalUsers()
-    const dataPosts = await fetchPosts({ pageSize: 4 })
+    const [users, posts] = await Promise.all([
+      fetchData.totalUsers(),
+      fetchData.posts({ pageSize: 4 }),
+    ])
 
     return (
       <section className={styles.publicPage}>
-        <TotalUsers totalCount={dataTotalUser.totalCount} />
-        <Posts posts={dataPosts.items} />
+        <TotalUsers totalCount={users.totalCount} />
+        <Posts posts={posts.items} />
       </section>
     )
   } catch {
