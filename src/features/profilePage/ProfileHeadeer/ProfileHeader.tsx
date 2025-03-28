822a65b9688ec8e3ec5ddd47553e9ad4c3bb4afa
Natalia <natalia21080@yahoo.de>
2025-03-28 11:19:05 +0100

    KAN-103: add Avatarfeatures and styles

--- src/features/profilePage/ProfileHeadeer/ProfileHeader.tsx
+++ src/features/profilePage/ProfileHeadeer/ProfileHeader.tsx
@@ -25,10 +25,8 @@ const ProfileHeader = ({ profileUser }: Props) => {
             alt={`UserPhoto`}
             className={styles.avatar}
             fill
-            height={204}
             priority
             src={avatars.length ? avatars[0].url : ''}
-            width={204}
           />
         ) : (
           <div className={styles.avatar}>фото нет</div>
@@ -42,10 +40,8 @@ const ProfileHeader = ({ profileUser }: Props) => {
                 alt={`UserPhoto`}
                 className={styles.avatar}
                 fill
-                height={204}
                 priority
                 src={avatars.length ? avatars[0].url : ''}
-                width={204}
               />
             ) : (
               <div className={styles.avatar}>фото нет</div>
