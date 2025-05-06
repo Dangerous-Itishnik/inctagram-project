6d04196c8798e8413d7e1fb9bd96a0d406a4252e
Natalia <natalia21080@yahoo.de>
2025-05-06 11:51:26 +0200

    prod add prod url

--- src/common/api/common.api.ts
+++ src/common/api/common.api.ts
@@ -1,1 +1,5 @@
-export const BASE_URL = 'https://inctagram.work'
+export const WORK_URL = 'https://inctagram.work'
+
+export const PROD_URL = 'https://dang-inc-project.uk/'
+
+export const BASE_URL = process.env.NODE_ENV === 'production' ? PROD_URL : WORK_URL
